2fe423d35cb56bd3dff756475b045ed1c0586213
YolandaFan <31786653+YolandaFan@users.noreply.github.com>
2017-10-06 12:37:45 +0800

    Add files via upload

---
+++ src/ems/Sender.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface Sender {
+   public Order AskToCreateOrder(String itemName,Customer c);
+    public Order AskToCreateOrder(String itemName,String address);
+   public String getCurrentLocation(Order o);
+   public Boolean ChangeDestination(Order o,String address);
+   public Boolean AskToWithdrawOrder(Order o);
+
+}
