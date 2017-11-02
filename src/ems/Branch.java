99ab570e7f40b0c92562bd0542ec7b8291f85479
hachiman hikigaya <wfz0755@hotmail.com>
2017-11-02 20:59:50 +0800

    Merge pull request #6 from patrickwang96/wfz
    Completed some function in branch class and added a new class
    OrderCollection.

--- src/ems/Branch.java
+++ src/ems/Branch.java
@@ -3,77 +3,144 @@ import java.util.*;
 //import java.util.zip.CheckedOutputStream;
 
 //import javax.print.attribute.standard.PrinterMessageFromOperator;
-
+// wfz &qr
 
 public class Branch {
+
+
 	private int id;
 	private String name;
-	private static int count=0;
-	private ArrayList<Order> packages = new ArrayList<Order>();
+	private int[] location;
+	private ArrayList<Branch> neighbour= new ArrayList<Branch>();
 	private ArrayList<Courier> freeMan = new ArrayList<Courier>();
 	private ArrayList<Courier> outMan = new ArrayList<Courier>();
+	private ArrayList<OrderCollection> packages = new ArrayList<OrderCollection>();
 	private ArrayList<Order> onDelivery = new ArrayList<Order>();
-	private static Object map = new Object() ;//to be confirm
-	
-	private static Branch[] optimizePath(Order p) {
-
-		return new Branch[10]; // tmp dummy return value -- added by patrick ;)
-	}
-
-	public Branch(int id, String name) {
+	private final int capacity;
+	private int robinpointer=0;
+	public Branch(int id, String name, int[] loc,int cap, int sur, ArrayList<Branch> branches) {
 //		TODO: conflict constructor with Manager
 		this.id = id;
 		this.name =name;
-		Branch.count++;
-		this.id=Branch.count;
+		this.location =loc;
+		this.capacity=cap;
+		this.neighbour=branches;
+		for(int i=0;i<=this.neighbour.size();i++){
+			this.packages.add(new OrderCollection(1)); // we need to initiate with unique id for each neighbor
+		}
 	}
 
-	public void checkIn(Order p,Branch previous) {
-		this.packages.add(p);
-		previous.arrive(p);
-	}
-
-	public void arrive(Order p) {
-		this.onDelivery.remove(p);
-	}
-
-	private void checkedOut(Order p, Courier m) {
-		this.packages.remove(p);
-		this.outMan.add(m);
-		this.freeMan.remove(m);
-		this.onDelivery.add(p);
-		//m.addParcel()
-	}
-
-	public void deliver(Order p, Courier m) {
-		//p.getAddress()
-		Branch next=Branch.optimizePath(p)[1];
-		this.checkedOut(p, m);
-		//m.send(p)
-	}
-	
-	public Courier getMan(int id) {
-/* TODO: implement getMan*/
-		return null;
-	}
-	
-	public Order getPkt(int id) {
-/* TODO: implement getOrder*/
-		return null;
-	}
 	public int getId() {
 		return this.id;
 	}
-	public void printPath(Order p){
-//		TODO: implement printPath in Branch
-//		prePath=p.getPrePath();
-//		Branch[] nextPath = Branch.optimizePath(p);
-//		path=prePath+nextPath;
-//		System.out.println(path);
-//		what ever...
-		
+
+	public String getName(){
+		return this.name;
 	}
-	
-	
-	
+
+	public Order getOrder(int id) {
+/* TODO: implement getOrder*/
+		for (OrderCollection q : this.packages) {
+			for (Order o : q.getQueue())
+			{
+				if(o.getId()==id)
+				{
+					return o;
+				}
+			}
+		}
+		for (Order order : this.onDelivery) {
+			if(order.getId() == id)
+			{
+				return order;
+			}
+		}
+		return null;
+	}
+
+	public Courier getMan(int id) {
+/* TODO: implement getMan*/
+		for (Courier m : this.freeMan) {
+			if(m.getId() == id)
+			{
+				return m;
+			}
+		}
+		for (Courier m : this.outMan) {
+			if(m.getId() == id)
+			{
+				return m;
+			}
+		}
+
+		return null;
+	}
+
+
+	public void checkInOrder(Courier c) {
+		for(OrderCollection q:this.packages){
+			if(checkLastDelivery(c.getOrder())){
+				if(checkLastDelivery(q.peek())){
+					q.add(c.getOrder());
+				}
+				else continue;
+			}
+			if(q.peek().getPath()[q.peek().getCurrenLocation()+1]==c.getOrder().getPath()[c.getOrder(0).getCurrenLocation()+1]){
+				q.add(c.getOrder())
+			}
+		}
+		c.getOrder().getPath()[getCurrenLocation()].arrive(c);
+	}
+
+	private void checkedOut(Courier c) {
+		ArrayList thingsToSend<Oreder>=new ArrayList<Order>();
+		int weight=this.packages.get(robinpointer).peek().getWeight();
+		while(weight<c.getCapacity()){
+			for (int i=0;i<this.packages.size();i++){
+				robinpointer=(robinpointer+1)%this.packages.size();
+				Order p=this.packages.get(robinpointer).peek();
+				if(p!=null) {
+					thingsToSend.add(p);
+					this.onDelivery.add(p);
+					this.packages.get(robinpointer).remove(p);
+					break;
+				}
+			}
+		}
+		this.outMan.add(c);
+		this.freeMan.remove(c);
+		if (thingsToSend.size()==0){
+			System.out.println("no package");
+		}
+		else if(checkLastDelivery(thingsToSend.peek())){
+			c.sendToHome(thingsToSend);
+		}
+		else{
+			c.sendToBranch(thingsToSend);
+		}
+	}
+
+	public void arrive(Courier c) {
+		this.onDelivery.remove(c.getOrder());
+		this.outMan.remove(c);
+		this.freeMan.add(c);
+	}
+	private boolean checkLastDelivery(Order o){
+		if (Math.abs(o.getAddress()[0]-this.location[0])+Math.abs(o.getAddress()[1]-this.location[1])<=this.capacity){
+			return true;
+		}
+		else return false;
+	}
+
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	public void updateNeighbour(Branch b){
+		this.neighbour.add(b);
+		this.packages.add(new OrderCollection(1)) ;// with new id
+	}
+
+
 }
