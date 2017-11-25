97ba9e9d75947856228ca11053d8eb6596ef87f1
SherlockQiu <Sherlockqiu007@gmail.com>
2017-11-25 20:52:58 +0800

    add two function for add linkage and rm linkage
    add two functions for add linkage and rm linkage and detect connection

---
+++ CmdSwitchToCustomer.java
@@ -0,0 +1,18 @@
+package ems;
+
+public class CmdSwitchToCustomer implements Command {
+	private Customer reference;
+	private int id;
+	private Customer customer;
+	public CmdSwitchToCustomer(Company company, String[] cmd, Customer theCustomer){
+		this.reference=theCustomer;
+		this.id=Integer.parseInt(cmd[1]);
+		this.customer=company.getCustomer(this.id);
+	}
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		this.reference=this.customer;
+	}
+
+}
