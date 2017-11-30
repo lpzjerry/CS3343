c6b21f526b1741b1f85a7c67e324ff4866c2ec10
Pengze Liu <lpzjerry@gmail.com>
2017-11-30 12:32:21 +0800

    Update CmdSearchOrder.java
    Handle null pointer exception

--- src/ems/CmdSearchOrder.java
+++ src/ems/CmdSearchOrder.java
@@ -14,9 +14,12 @@ public class CmdSearchOrder implements Command {
         // Order order = this.theCompany.searchOrder(Integer.parseInt(param[1]));
         String orderKey = param[1];
         Order order = theCompany.searchOrder(orderKey);
-        if(order == null)
-            order = theCompany.searchOrder(Integer.parseInt(orderKey));
-        if (order.isReceived()) System.out.println("Order " + order + " has been received.");
+        if (order == null) // Search by Order Name
+            order = theCompany.searchOrder(Integer.parseInt(orderKey)); // search by ID
+        if (order == null) // Order not found
+            System.out.println("Order not found. Please input the correct Order ID or Order Name");
+        else if (order.isReceived()) 
+            System.out.println("Order " + order + " has been received.");
         else {
             System.out.println(order);
             Position locOfOrder = order.currentLocation();
