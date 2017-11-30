555c8c993dbb7f344dc52715109ab99290d1710d
SheilaCecilia <djyzhwy@gmail.com>
2017-11-30 13:59:00 +0800

    Add files via upload

---
+++ src/ems_test/CmdAddManagerTest.java
@@ -0,0 +1,40 @@
+package ems_test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ems.CmdAddManager;
+import ems.Company;
+import ems.Customer;
+import ems.Position;
+import ems.Manager;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class CmdAddManagerTest {
+	Manager manager = null;
+	String[]para = null;
+	CmdAddManager cmd = null;
+	@Before
+	public void setUp() throws Exception {
+		Company company = Company.getInstance();
+		company.addNewManager("super", "hahaha", "Male", 0);
+		manager = Company.getInstance().getManager(0);
+		para = new String[]{"addManager","Sheila","abc","F","666"};
+		cmd = new CmdAddManager(manager,para);
+	}
+
+	@Test
+	public void testExecute() {
+		 ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	     System.setOut(new PrintStream(outContent));
+	     cmd.execute();
+	     assertEquals("Express Company Manager Sheila (level:666) created!\n[manager Sheila] is added by [manager super]\n",outContent.toString());
+	}
+
+}
---
+++ src/ems_test/CmdCheckTimeTest.java
@@ -0,0 +1,19 @@
+package ems_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CmdCheckTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+}
