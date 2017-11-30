1db371bf04e927f56ddcefad8cf664310ab8f7aa
SheilaCecilia <djyzhwy@gmail.com>
2017-11-30 14:17:36 +0800

    Update CmdCheckTimeTest.java

--- src/ems_test/CmdCheckTimeTest.java
+++ src/ems_test/CmdCheckTimeTest.java
@@ -2,18 +2,31 @@ package ems_test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class CmdCheckTimeTest {
+import ems.CmdCheckTime;
+import ems.Company;
 
-	@Before
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+public class CmdCheckTimeTest {
+    CmdCheckTime cmd = null;
+	String[] para;
+    @Before
 	public void setUp() throws Exception {
+		para = new String[] {"checkTime"};
+    		cmd = new CmdCheckTime(Company.getInstance(),para);
 	}
 
 	@Test
 	public void testExecute() {
-		fail("Not yet implemented");
+		 ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	     System.setOut(new PrintStream(outContent));
+	     cmd.execute();
+	     assertEquals("It is 1\n",outContent.toString());
 	}
 
 }
