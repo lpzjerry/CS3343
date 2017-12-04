7c962fda2cf4987dc31cee668c313f2011f2c885
SheilaCecilia <djyzhwy@gmail.com>
2017-12-04 16:17:27 +0800

    Update Company.java
    Doc Comments for the Javadoc Tool

--- src/ems/Company.java
+++ src/ems/Company.java
@@ -1,5 +1,12 @@
 package ems;
-
+/**
+ * <h1>Company</h1>
+ * The company contains all the overall information about the system, including
+ * the managerlist, branchlist, customerlist, the map of the delivery system. And 
+ * it can modify the information via calling the methods.
+ *@version v0.2-beta
+ *
+ * */
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -15,7 +22,12 @@ public class Company {
     private int branchId;
     private OrderPool orderPool;
     private Date companyClock;
-
+    /**
+     * This method is used to add linkage between branches to the company.
+     * @param source The id of the source branch.
+     * @param target The id of the target branch.
+     * @return Whether the linkage is successfully added.
+     * */
     public boolean addLinkage(int source, int target) {
         if (this.branchList.containsKey(source) && this.branchList.containsKey(target)) {
             int distance = this.branchList.get(source).getDistance(this.branchList.get(target));
@@ -26,6 +38,9 @@ public class Company {
         System.out.println("source or target does not exist");
         return false;
     }
+    /**
+     * This method is reset the company.
+     * */
     public void reset() {
     		this.orderPool.reset();;
          this.branchList.clear();
@@ -39,6 +54,12 @@ public class Company {
          this.map = new Graph(new ArrayList<Branch>(), this.edges);
          this.branchId = 1;
     }
+    /**
+     * This method is used remove linkage from the company's map.
+     * @param source The id of the source branch.
+     * @param target The id of the target branch.
+     * @return Whether the linkage is successfully removed.
+     * */
     public boolean rmLinkage(int source, int target) {
         ArrayList<Edge> edgeToRemove = new ArrayList<Edge>();
         if (this.branchList.containsKey(source) && this.branchList.containsKey(target)) {
@@ -61,9 +82,8 @@ public class Company {
 //    public boolean connectOrNot(int source, int target){
 //    	return this.adjacency.get(source).contains(target)&&this.adjacency.get(target).contains(source);
 //    }
-
     /**
-     * while company's init, create a user with full priviledges.
+     *This is the company constructor. 
      */
     private Company() {
         this.orderPool = OrderPool.getInstance();
@@ -80,18 +100,34 @@ public class Company {
     }
 
     private static Company instance = new Company();
-
+    /**
+     *This is the company get singleton instance method.
+     *@return The singleton instance.
+     */
     public static Company getInstance() {
         return instance;
     }
-
+    /**
+     * This method is used to add manager to the company.
+     * @param name The name of the manager to add.
+     * @param password The password for the manager to log in.
+     * @param gender The gender of the manager.
+     * @param status The status of the manager to add.
+     * @return The manager which is added.
+     * */
     public Manager addNewManager(String name, String password, String gender, int status) {
         Manager manager = new Manager(managerList.size(), name, password, gender, status);
 
         this.managerList.put(manager.getId(), manager);
         return manager;
     }
-
+    /**
+     * This method is used to create new orders and plan the route of delivery automatically.
+     * @param itemName The name of the order.
+     * @param sender The sender customer.
+     * @param receiver The receiver customer.
+     * @return The new order's id or the error information.
+     * */
     public int createOrder(String itemName, Customer sender, Customer receiver) {
         ArrayList<Position> path = new ArrayList<>();
         Position s = sender.getPosition();
@@ -140,7 +176,12 @@ public class Company {
     //    }
         return this.orderPool.createOrder(itemName, sender, receiver, path);
     }
-
+    /**
+     * This method is used to add a new branch to the company.
+     * @param name The name of the branch.
+     * @param position The position of the branch.
+     * @return The branch which is added.
+     * */
     public Branch addBranch(String name, Position position) {
         int id = this.branchId++;
         Branch branch = new Branch(id, name, position);
@@ -153,7 +194,11 @@ public class Company {
         this.map = new Graph(bList, this.edges);
         return branchList.put(id, branch);
     }
-
+    /**
+     * This method is used to remove a branch from the company.
+     * @param id The id of the branch.
+     * @return The branch which is removed.
+     * */
     public Branch removeBranch(int id) {
 
         Branch toRemove = branchList.remove(id);
@@ -178,38 +223,69 @@ public class Company {
 
         return toRemove;
     }
-
+    /**
+     * This method is used to search the order by id.
+     * @param id The id of the order.
+     * @return The order which is searched.
+     * */
     public Order searchOrder(int id) {
         return orderPool.getOrderById(id);
     }
-
+    /**
+     * This method is used to search the order by name.
+     * @param name The name of the order.
+     * @return The order which is searched.
+     * */
     public Order searchOrder(String name) {
         return orderPool.getOrderByName(name);
     }
 
+    /**
+     * This method is used to change the status of a order after it has been received.
+     * @param order The order which is received.
+     * */
     public void receiveOrder(Order order) {
         orderPool.receiveOrder(order);
     }
 
     /**
-     * @return type:long, return the time after the company is created(in millisecond)
+     * This method is used to get the time of the company after creation.
+     * @return The time after the company is created(in millisecond)
      */
     public long getTime() {
         return new Date().getTime() - this.companyClock.getTime();
     }
+    
+    /**
+     * This method is used to add customer to the company.
+     * @param name The name of the customer to add.
+     * @param password The password for the customer to log in.
+     * @param priority The priority of the customer.
+     * @param position The position of the customer.
+     * @return The customer which is added.
+     * */
 
     public Customer addCustomer(String name, String password, int priority, Position position) {
         Customer customer = new Customer(customerList.size(), name, password, priority, position);
         this.customerList.put(customer.getId(), customer);
         return customer;
     }
-
+    /**
+     * This method is used to add customer to the company.
+     * @param name The name of the customer to add.
+     * @param password The password for the customer to log in.
+     * @return The customer which is added.
+     * */
     public Customer addCustomer(String name, String password) {
         Customer customer = new Customer(customerList.size(), name, password);
         this.customerList.put(customer.getId(), customer);
         return customer;
     }
-
+    /**
+     * This method is used to search the branch by position.
+     * @param position The position of the branch.
+     * @return The branch which is searched.
+     * */
     public Branch getBranchByLocation(Position position) {
 
         Map<Integer, Branch> tmp_container = this.branchList.entrySet().stream()
@@ -219,15 +295,27 @@ public class Company {
 //        return tmp_container to get all branches on this location.
         return tmp_container.entrySet().iterator().next().getValue();
     }
-
+    /**
+     * This method is used to search the manager by id.
+     * @param id The id of the manager.
+     * @return The manager which is searched.
+     * */
     public Manager getManager(int id) {
         return this.managerList.get(id);
     }
-
+    /**
+     * This method is used to remove the manager by id.
+     * @param id The id of the manager.
+     * @return The manager which is removed.
+     * */
     public Manager removeManager(int id) {
         return this.managerList.remove(id);
     }
-
+    /**
+     * This method is used to get the customer by id.
+     * @param id The id of the customer.
+     * @return The customer which is required.
+     * */
     public Customer getCustomer(int id) {
         return this.customerList.get(id);
     }
